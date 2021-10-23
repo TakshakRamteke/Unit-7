139bb26a85a426a2855a21f5f343d49343e4b97e
TakshakRamteke <takshakramteke0708@gmail.com>
2021-10-23 13:14:23 +0530

    feat : Folder structure and base function added

---
+++ src/u7.py
@@ -0,0 +1,12 @@
+try :
+    import usocket as socket
+except :
+    import socket
+
+class U7():
+
+    def __init__(self, ssid, password):
+
+        self.ssid = ssid
+        self.password = password
+
