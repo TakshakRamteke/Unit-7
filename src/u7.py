1e60dbafebfa064be1ac68f0715903ad474f4a76
Harsh <harshmaroo123@gmail.com>
2021-10-23 14:17:09 +0530

    html rendering function added

--- src/u7.py
+++ src/u7.py
@@ -10,3 +10,8 @@ class U7():
         self.ssid = ssid
         self.password = password
 
+    def html_to_string(file_location):
+        with open(file_location, "r", encoding='utf-8') as f:
+            text= f.read()
+        f.close()
+        return text
