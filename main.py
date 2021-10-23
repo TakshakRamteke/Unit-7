aca74d70b94c8c01b682fc23343a2d75ce19c1b4
TakshakRamteke <takshakramteke0708@gmail.com>
2021-10-23 20:53:04 +0530

    feat : Added function to create server and serve the given html file

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
 from machine import Pin
-import time
-import network
-
-def toggle(p):
-    p.value(not p.value())
+from u7 import U7
 
 
-led = Pin(2,Pin.OUT) 
+led = Pin(2,Pin.OUT)
 
-ssid = str(input("Please enter your SSID : "))                  
-password = str(input("Please enter Password for this SSID : "))    
-
-station = network.WLAN(network.STA_IF)
-station.active(True)
-station.connect(ssid, password)
-
-while station.isconnected == False :
-    print("trying to connect",end="")
-    toggle(led)
-    time.sleep(1)
-    
-    if station.isconnected == True :
-        led = Pin(2,Pin.OUT)
-        station.ifconfig()
-        break
-
-    else :
-        print(".",end="")
+app = U7
+app.run(app.render())
--- src/u7.py
+++ src/u7.py
@@ -5,13 +5,25 @@ except :
 
 class U7():
 
-    def __init__(self, ssid, password):
+    def __init__(self):
+        pass
 
-        self.ssid = ssid
-        self.password = password
+    def render():
+        with open("/examples/test.html", "r", encoding='utf-8') as file:
+            html = file.read()
+        file.close()
+        return html
+    
+    def run(html):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('', 80))
+        server.listen(5)
+        conn, addr = server.accept()
+        print('Received HTTP GET connection request from %s' % str(addr))
+        response = html
+        conn.send('HTTP/1.1 200 OK\n')
+        conn.send('Content-Type: text/html\n')
+        conn.sendall(response)
+        conn.close()
 
-    def html_to_string(file_location):
-        with open(file_location, "r", encoding='utf-8') as f:
-            text= f.read()
-        f.close()
-        return text
+
